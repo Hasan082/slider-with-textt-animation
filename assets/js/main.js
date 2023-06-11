afe0661fbeab86f72f4b675588b65c11753a4a58
hasan082 <dr.has82@gmail.com>
2023-06-11 13:40:49 +0600

    Owl Carousel with text animation

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    nav: true,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 7000,
+    smartSpeed: 1000,
+    navText: [
+      '<img src="./assets/images/prev-white.png" alt="">',
+      '<img src="./assets/images/next-white.png" alt="">'
+    ],
+  })
+});
